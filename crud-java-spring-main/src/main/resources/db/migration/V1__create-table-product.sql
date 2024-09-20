6bfdaf79f31bc417377649a6b1ee0d67e69b781f
Pedro Ferreira Martins <100172927+Pedro-FMartins@users.noreply.github.com>
2024-09-20 13:25:01 -0300

    Integration with the "viacep" API

--- crud-java-spring-main/src/main/resources/db/migration/V1__create-table-product.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE product (
-    id TEXT PRIMARY KEY UNIQUE NOT NULL,
-    name TEXT NOT NULL,
-    price INT NOT NULL,
-    description TEXT NOT NULL
-);
---
+++ crud_products_integration_viacep/src/main/resources/db/migration/V6__update-table-product.sql
@@ -0,0 +1,11 @@
+UPDATE product
+SET distribution_center = 'Mogi das Cruzes'
+WHERE id IN ('p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7');
+
+UPDATE product
+SET distribution_center = 'Ferraz de Vasconcelos'
+WHERE id IN ('p8', 'p9', 'p10', 'p11', 'p12', 'p13', 'p14');
+
+UPDATE product
+SET distribution_center = 'Guarulhos'
+WHERE id IN ('p15', 'p16', 'p17', 'p18', 'p19', 'p20');
